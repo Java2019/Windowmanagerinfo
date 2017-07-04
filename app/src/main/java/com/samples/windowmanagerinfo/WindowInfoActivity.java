607ec9d12e8bad1386684d2653c092184a45c4d6
java2019 <java_2019@mail.ru>
2017-07-04 16:59:39 +0300

    final

--- app/src/main/java/com/samples/windowmanagerinfo/WindowInfoActivity.java
+++ app/src/main/java/com/samples/windowmanagerinfo/WindowInfoActivity.java
@@ -1,7 +1,12 @@
 package com.samples.windowmanagerinfo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class WindowInfoActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class WindowInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_window_info);
+        TextView text = (TextView)findViewById(R.id.text);
+        WindowManager manager =
+                (WindowManager)getSystemService(Context.WINDOW_SERVICE);
+        Display display = manager.getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+        text.append("Density:\t" + metrics.density);
+        text.append("\nWidth pixels:\t" + metrics.widthPixels);
+        text.append("\nHeight pixels:\t" + metrics.heightPixels);
+        text.append("\nDensity dpi:\t" + metrics.densityDpi);
+        text.append("\nX dpi:\t" + metrics.xdpi);
+        text.append("\nY dpi:\t" + metrics.ydpi);
+        text.append("\nScaled density:\t" + metrics.scaledDensity);
     }
 }
